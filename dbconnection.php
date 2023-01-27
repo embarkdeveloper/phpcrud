6731c5f849f21eb42622d75518c85c750ef70722
embarkdeveloper <91586071+EmbarkEBS@users.noreply.github.com>
2023-01-27 18:50:33 +0530

    File modified

--- dbconnection.php
+++ dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("localhost", "root", "", "phpbasiccrud");
+$con=mysqli_connect("localhost", "techdemy_crud", "techdemypwd", "techdemy_crud");
 if(mysqli_connect_errno())
 {
 echo "Connection Fail".mysqli_connect_error();
